b82f0450f0ebc78eeda309f6c55b842bf2bffb47
Elissa Olivas <elissaolivas@Elissas-MacBook-Pro.local>
2022-03-23 12:51:52 -0600

    Adding in unsuccessful test

--- src/test/java/com/detroitlabs/LotusYogaStudio/data/ClassesRepositoryTest.java
+++ src/test/java/com/detroitlabs/LotusYogaStudio/data/ClassesRepositoryTest.java
@@ -1,16 +1,24 @@
-package com.detroitlabs.LotusYogaStudio.data;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ClassesRepositoryTest {
-
-    @Test
-    void listClassByName() {
-    }
-
-    @Test
-    void listClassByTime() {
-    }
-}
+//package com.detroitlabs.LotusYogaStudio.data;
+//
+//import com.detroitlabs.LotusYogaStudio.Model.Classes;
+//import org.junit.jupiter.api.Test;
+//
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class ClassesRepositoryTest {
+//
+//    @Test
+//    void listClassByName() {
+//        ClassesRepository classesRepository = new ClassesRepository();
+//        Classes results= classesRepository.listClassByTime("09:00");
+//            assertEquals(results.getTimeOfClass().equals("09:00"));
+//        }
+//
+//
+//
+//    @Test
+//    void listClassByTime() {
+//    }
+//}

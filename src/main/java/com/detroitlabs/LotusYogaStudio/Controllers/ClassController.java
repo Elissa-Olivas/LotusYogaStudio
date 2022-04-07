832c19f65f4c12d3e9232582622fc576adc302f2
Elissa Olivas <elissaolivas@Elissas-MacBook-Pro.local>
2022-04-07 06:27:25 -0600

    Trying meta tag for github pages

--- src/main/java/com/detroitlabs/LotusYogaStudio/Controllers/ClassController.java
+++ src/main/java/com/detroitlabs/LotusYogaStudio/Controllers/ClassController.java
@@ -24,7 +24,7 @@ public class ClassController {
     //HOME PAGE
     @RequestMapping("/")
     public String displayHome() {
-        return "home";
+        return "index";
     }
 
     //CLASSES PAGE
